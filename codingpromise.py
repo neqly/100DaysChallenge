1bd8c7bf07421e4755255faa3448c32d929fe130
neqly <noahhhgathings@gmail.com>
2024-12-19 18:39:08 -0500

    Coding promise loop

---
+++ codingpromise.py
@@ -0,0 +1,5 @@
+def basic_loop(name):
+    for n in range(5):
+        print("Hi, " + name + "! I'm coonfident you will complete the course if you are consistent with coding every day.\n")
+
+basic_loop("Noah and Seth")
